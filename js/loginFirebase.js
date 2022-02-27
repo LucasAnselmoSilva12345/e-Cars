50a2b89690475dc6f7702c91f4f5f056c22362e7
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-02-27 19:26:42 -0300

    [WIP] Adds login with email, google, facebook to firebase.
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- js/loginFirebase.js
+++ js/loginFirebase.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
 import {
   getAuth,
@@ -6,18 +5,17 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
 } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 import {
   getFirestore,
-  collection,
   doc,
   onSnapshot,
 } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAbm1ipG8POxNTKFzwLvciyW817NeY--4c',
   authDomain: 'ecarsmultilogin.firebaseapp.com',
@@ -27,88 +25,123 @@ const firebaseConfig = {
   appId: '1:25355814160:web:a60e4a82f06cf8bb725195',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const auth = getAuth();
+const auth = getAuth();
+const containerModal = document.querySelector('[data-modal="container"]');
 
+// SIGNUP
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (event) => {
   event.preventDefault();
-
   const signupEmail = document.querySelector('#signup-email').value;
   const signupPassword = document.querySelector('#signup-password').value;
 
-  const containerModal = document.querySelector('[data-modal="container"]');
+  createUserWithEmailAndPassword(auth, signupEmail, signupPassword).then(() => {
+    signupForm.reset();
+    containerModal.classList.remove('activeModal');
 
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, signupEmail, signupPassword).then(
-    (userCredential) => {
-      signupForm.reset();
-      containerModal.classList.remove('activeModal');
-      console.log('Enviando');
-    }
-  );
+    alert('Usuário criado');
+  });
 });
 
+// SIGNIN
 const signinForm = document.querySelector('#signin-form');
-
 signinForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const signinEmail = document.querySelector('#signin-email').value;
   const signinPassword = document.querySelector('#signin-password').value;
+  const registerBtn = document.querySelector('.register-btn');
 
-  const containerModal = document.querySelector('[data-modal="container"]');
-
-  const auth = getAuth();
   signInWithEmailAndPassword(auth, signinEmail, signinPassword).then(
     (userCredential) => {
       signinForm.reset();
       containerModal.classList.remove('activeModal');
-      console.log('SIGN IN');
+
+      const userEmail = userCredential.user.email;
+      registerBtn.innerHTML = `<a href="#" data-modal="open" class="register-btn">
+                                <i class="gg-profile"></i>
+                                <span>${userEmail}</span>
+                              </a>`;
     }
   );
 });
 
+// LOGOUT
 const logout = document.querySelector('#logout');
-
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-
-  const auth = getAuth();
   signOut(auth).then(() => {
-    console.log('Sign-out successful');
+    containerModal.classList.remove('activeModal');
+
+    const registerBtn = document.querySelector('.register-btn');
+    registerBtn.innerHTML = `<a href="#" data-modal="open" class="register-btn">
+                                <i class="gg-profile"></i>
+                                Faça seu login
+                              </a>`;
   });
 });
 
-// const containerContent = document.querySelector('.containerContent');
+// LOGIN TO GOOGLE
+const btnLoginToGoogle = document.querySelector('#btnLoginToGoogle');
+btnLoginToGoogle.addEventListener('click', () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const userDisplayName = result.user.displayName;
+
+      const registerBtn = document.querySelector('.register-btn');
+      registerBtn.innerHTML = `<a href="#" data-modal="open" class="register-btn">   
+                                <span>${userDisplayName}</span>
+                              </a>`;
+
+      containerModal.classList.remove('activeModal');
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
+
+// LOGIN TO FACEBOOK
+const btnLoginToFacebook = document.querySelector('#btnLoginToFacebook');
+btnLoginToFacebook.addEventListener('click', () => {
+  const provider = new FacebookAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const userDisplayName = result.user.displayName;
+
+      const registerBtn = document.querySelector('.register-btn');
+      registerBtn.innerHTML = `<a href="#" data-modal="open" class="register-btn">   
+                                <span>${userDisplayName}</span>
+                              </a>`;
+
+      containerModal.classList.remove('activeModal');
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
+
+// verificar autenticação
 const titleContent = document.querySelector('.titleContent h2');
 const mainContent = document.querySelector('.mainContent');
 
-// verificar autenticação
-const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const db = getFirestore();
-    // const salesCollection = collection(db, 'sales');
-    // const salesCollection = doc(db, 'sales/S9INqtZ0OwHD35SkvT8d');
 
-    const salesCollection = onSnapshot(
-      doc(db, 'sales', 'S9INqtZ0OwHD35SkvT8d'),
-      (doc) => {
-        console.log(doc.data().title);
-        console.log(doc.data().description);
+    onSnapshot(doc(db, 'sales', 'S9INqtZ0OwHD35SkvT8d'), (doc) => {
+      titleContent.innerHTML = doc.data().title;
+      mainContent.innerHTML = doc.data().description;
 
-        titleContent.innerHTML = doc.data().title;
-        mainContent.innerHTML = doc.data().description;
-      }
-    );
-
-    console.log('auth signIn');
+      // adicionar class de display block para aparecer opções de venda
+    });
   } else {
     titleContent.innerHTML = 'Um momento meu caro vendendor';
     mainContent.innerHTML =
       'Para realizar uma venda em nosso site, é necessario fazer sua autenticação primeiro';
-    console.log('auth signOut');
   }
 });
