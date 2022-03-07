0a2af46aa99c3027671bb719ee36c884d0e76a86
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-03-07 13:45:20 -0300

    Remove comment in script.js
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ import { initMenuMobile } from './module/initMenuMobile.js';
 import { initModal } from './module/initModal.js';
 import { initTransitionFormModal } from './module/initTransitionFormModal.js';
 import { initTooltip } from './module/initTooltip.js';
-// import { initOpenModal } from './module/initOpenForm.js';
+import { initOpenModal } from './module/initOpenForm.js';
 import AccordionList from './module/initAccordionList';
 
 initMenuMobile();
 initModal();
 initTransitionFormModal();
 initTooltip();
-// initOpenModal();
+initOpenModal();
 
 const initAccordionList = new AccordionList('[data-anime="accordion"] dt');
 initAccordionList.init();
